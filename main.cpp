bf64bd79380cf1753a5be0706f46011e3864ac9d
kura495 <104417866+kura495@users.noreply.github.com>
2023-04-27 01:32:45 +0900

    変更

--- main.cpp
+++ main.cpp
@@ -5,15 +5,6 @@
 
 const char kWindowTitle[] = "LE2B_11_クラモト_アツシ_MT3";
 
-Vector3 Add(const Vector3& v1,const Vector3& v2);
-Vector3 Subtract(const Vector3& v1, const Vector3& v2);
-Vector3 Multiply(float scalar, const Vector3& v);
-float Dot(const Vector3& v1, const Vector3& v2);
-float Length(const Vector3& v);
-Vector3 Normalize(const Vector3& v);
-const int kColumnWidth = 60;
-const int kRowHeight = 20;
-void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -24,10 +15,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-
-	Vector3 v1{1.0f,3.0f,-5.0f};
-	Vector3 v2{4.0f,-1.0f,2.0f};
-	float k = 4.0f;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -42,12 +29,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Vector3 resultAdd = Add(v1, v2);
-		Vector3 resultSubtract = Subtract(v1, v2);
-		Vector3 resultMultiply = Multiply(k, v1);
-		float resultDot = Dot(v1, v2);
-		float resultLength = Length(v1);
-		Vector3 resultNormalize = Normalize(v2);
+
 
 		///
 		/// ↑更新処理ここまで
@@ -57,12 +39,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, resultAdd," : Add");
-		VectorScreenPrintf(0,kRowHeight, resultSubtract," : Subtract");
-		VectorScreenPrintf(0, kRowHeight*2, resultMultiply," : Multiply");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f : Length", resultLength);
-		VectorScreenPrintf(0, kRowHeight*5, resultNormalize," : Normalize");
 
 		///
 		/// ↑描画処理ここまで
@@ -80,44 +56,4 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの終了
 	Novice::Finalize();
 	return 0;
-}
-
-Vector3 Add(const Vector3& v1, const Vector3& v2)
-{
-
-	return Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-}
-
-Vector3 Subtract(const Vector3& v1, const Vector3& v2)
-{
-	return Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-}
-
-Vector3 Multiply(float scalar, const Vector3& v)
-{
-	return Vector3(v.x * scalar, v.y * scalar, v.z * scalar);
-}
-
-float Dot(const Vector3& v1, const Vector3& v2)
-{
-	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-}
-
-float Length(const Vector3& v)
-{
-	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-}
-
-Vector3 Normalize(const Vector3& v)
-{
-	float resultLength = Length(v);
-	return	Vector3 (v.x/ resultLength,v.y / resultLength,v.z / resultLength) ;
-}
-
-void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label)
-{
-	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
-	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
-	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
-	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
 }
