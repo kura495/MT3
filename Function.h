4942876f8599d0aa3eef3e8f78e67ad1fd09266e
kura495 <104417866+kura495@users.noreply.github.com>
2023-05-23 20:04:30 +0900

    a

--- Function.h
+++ Function.h
@@ -1,1 +1,7 @@
 #pragma once
+#include<Matrix4x4.h>
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
+	const float kGridHalfwidth = 2.0f;
+	const uint32_t kSubdivision = 10;
+
+}
